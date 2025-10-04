7af2aa1837b0c901e97e3731ccf20c5df4e3c522
Venkata Mallikarjuna <nvmalllikarjuna@gmail.com>
2025-10-04 11:43:13 +0530

    updated given number is even/odd

--- 11-even-odd.sh
+++ 11-even-odd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Please enter the number:"
-read NUMBER
+read -s NUMBER
 
-REMINDER=$(($NUMBER % 2))
+#REMINDER=$(($NUMBER % 2))
 
-if [ $REMINER -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given Number $NUMBER is even"
 else
     echo "Given Number $NUMBER is odd"
