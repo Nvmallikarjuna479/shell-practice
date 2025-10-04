6b032af305bbb5a522024e229a490a24c047ad8f
Venkata Mallikarjuna <nvmalllikarjuna@gmail.com>
2025-10-04 16:27:37 +0530

    installing package with functions

---
+++ 13-installing-multiple-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root previllages"
+    exit 1
+fi 
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: installing $2 is failed"
+        exit 1
+    else
+        echo "installing $2 is successful"
+    fi 
+}
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongodb-mongosh"
