47fe1427a2c56814db8d1afe4f89a84f4495a7ea
Venkata Mallikarjuna <nvmalllikarjuna@gmail.com>
2025-10-04 17:20:16 +0530

    fixing the collection logs conditions

--- 16-logs.sh
+++ 16-logs.sh
@@ -4,18 +4,19 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_DIR="/var/log/shell-script"
-SCRIPT_NAME=$(echo  $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_DIR/$SCRIPT_NAME.log"
-
-mkdir -p $LOGS_DIR
-echo "Script started executed at: $(date)"
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo  "ERROR:: Please run this script with root previllages"
     exit 1
 fi
+
+LOGS_DIR="/var/log/shell-script"
+SCRIPT_NAME=$(echo  $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_DIR/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_DIR
+echo "Script started executed at: $(date)"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
@@ -25,8 +26,9 @@ VALIDATE(){
         echo "installing $2 ... $G SUCCESS $N"
     fi 
 }
+
 dnf list installed mysql &>>$LOG_FILE
-if [ "$1" -ne "0" ]; then
+if [ $? -ne 0 ]; then
     dnf install mysql -y  &>>$LOG_FILE
     VALIDATE $? "mysql"
 else
@@ -34,7 +36,7 @@ else
 fi
 
 dnf list installed nginx  &>>$LOG_FILE
-if [ $1 -ne 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install nginx -y  &>>$LOG_FILE
     VALIDATE $? "nginx"
 else
@@ -42,7 +44,7 @@ else
 fi
 
 dnf list installed python3  &>>$LOG_FILE
-if [ $1 -ne 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install python3 -y  &>>$LOG_FILE
     VALIDATE $? "python3"
 else
