46059aa94f47b8e6c0f0255602a7bbe0078a9989
Venkata Mallikarjuna <nvmalllikarjuna@gmail.com>
2025-10-04 16:14:51 +0530

    installing package with script with exit status

--- 12-checking-exit-status.sh
+++ 12-checking-exit-status.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR: Please run this script with root previllages"
+    exit 1
 fi 
 
 dnf install mysql -y
@@ -11,4 +12,5 @@ if [ $? -eg 0 ]; then
     echo "installing mysql is successful"
 else
    echo "ERROR:: installing mysql is failed"
+   exit 1
 fi 
